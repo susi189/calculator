1996abbae145a4ec8e3b6b1fbeb34a8f63d9cb51
susi189 <ssteinhoff82@gmail.com>
2021-04-14 12:10:06 -0400

    Added changes to number key functionality and refactored operate function

--- script.js
+++ script.js
@@ -1,51 +1,21 @@
 // Create funtions for each operation
 
-function add(x, y){
-    return parseFloat(x) + parseFloat(y)
-}
-
-function subtract(x, y){
-    return parseFloat(x) - parseFloat(y)
-}
-
-function multiply(x, y){
-    return parseFloat(x) * parseFloat(y)
-}
-
-function divide(x, y){
-    return parseFloat(x)/parseFloat(y)
-}
-
-//here I will store single calculation elements that will be displayed
-// let lineUp = [];
-
-//here are the curent numbers that the calculation is performed on
-// let operationElements = {
-//     operator: null,
-//     number: null
-// }
-
-
 const operate = function(operator, num1, num2){ 
-let result = '';
+    const firstNum = parseFloat(num1);
+    const secondNum = parseFloat(num2);
+   if(operator === 'add'){
+       return firstNum + secondNum
+   }
    if(operator === 'subtract'){
-        result = subtract(num1, num2);
-    } else if(operator === 'multiply'){
-        result = multiply(num1, num2);
-    } else if(operator === 'divide'){
-        result = divide(num1, num2);
-    } else if(operator === 'add'){
-        result = add(num1, num2)
+        return firstNum - secondNum;
+    } 
+    if(operator === 'multiply'){
+        return firstNum * secondNum;
+    } 
+    if(operator === 'divide'){
+        return firstNum/secondNum
     }
-   return result
 }
-
-// const clear = function(){
-//     lineUp = [];
-//     operationElements = {operator: null, number: null};
-//     result = null;
-//     display.innerText = 0;
-// }
 
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.keys');
@@ -57,27 +27,28 @@ keys.addEventListener('click', (event) => {
     if(event.target.matches('button')){
         const key = event.target;
         let keyContent = key.innerText;
-        const action = key.getAttribute('data-action');
-        const displayedContent = display.innerText;
-        const previousKeyType = calculator.dataset.previousKeyType;
+        let action = key.getAttribute('data-action');
+        let displayedContent = display.innerText;
+        let previousKeyType = calculator.dataset.previousKeyType;
         let firstValue = calculator.dataset.firstValue;
         let operator = calculator.dataset.operator;
         let secondValue = displayedContent;
-
+        console.log(previousKeyType, firstValue, operator, secondValue)
         if(!action){
             //we have a number
-            console.log(previousKeyType)
-            if(displayedContent === '0' || previousKeyType === 'operator' || previousKeyType === 'equal'){
+            if(!firstValue || previousKeyType === 'operator'){
                 display.innerText = keyContent;
+            } else if(previousKeyType === 'calculate'){
+                display.innerText = keyContent;
+                calculator.dataset.firstValue = '';
+                calculator.dataset.operator = '';
             } else {
-                display.innerText = displayedContent + keyContent
+                display.innerText = displayedContent + keyContent;
             }
-             calculator.dataset.previousKeyType = 'number'
+             calculator.dataset.previousKeyType = 'number';
         } 
-        
         if(action === 'add' || action === 'subtract' || action === 'divide' || action === 'multiply'){
-
-            if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'equal') {
+            if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
                 const calculatedValue = operate(operator, firstValue, secondValue);
                 display.innerText = calculatedValue;
                 calculator.dataset.firstValue = calculatedValue;
@@ -88,16 +59,16 @@ keys.addEventListener('click', (event) => {
             calculator.dataset.previousKeyType = 'operator';
             calculator.dataset.operator = action;
         }
-        if(action === 'equal'){
+        if(action === 'calculate'){
             if(firstValue){
-                if(previousKeyType === 'equal'){
+                if(previousKeyType === 'calculate'){
                     firstValue = displayedContent;
                     secondValue = calculator.dataset.modValue;
                 }
                 display.innerText = operate(operator, firstValue, secondValue); 
             }
             calculator.dataset.modValue = secondValue;
-            calculator.dataset.previousKeyType = 'equal';
+            calculator.dataset.previousKeyType = 'calculate';
         }
         if(action === 'clear'){
             display.innerText = '0';
@@ -122,8 +93,8 @@ keys.addEventListener('click', (event) => {
             calculator.dataset.previousKeyType = 'decimal';
         }
         if(action === 'percent'){
-            
-            calculator.dataset.previousKeyType = 'percent'
+            display.innerText = displayedContent/100;
+            calculator.dataset.previousKeyType = 'percent';
         }
         if(action === 'negative'){ 
             if(!displayedContent.includes('-') && displayedContent !== '0'){
@@ -131,9 +102,8 @@ keys.addEventListener('click', (event) => {
             } else if(displayedContent.includes('-')) {
                 display.innerText = displayedContent.slice(1);
             }
-            calculator.dataset.previousKeyType = 'negative'
+            calculator.dataset.previousKeyType = 'negative';
         }
-        console.log(firstValue, secondValue)
     }
     
 });
@@ -142,84 +112,3 @@ keys.addEventListener('click', (event) => {
 // -> digit no longer than 9
 // -> is-depressed when user clicks the button
 
-
-
-
-// button.forEach((button) => {
-//     button.addEventListener('click', (event) => {
-//      //what will happen when a number is pressed
-//     let currentTarget = event.target.innerText;
-//     let currentNumber = operationElements.number;
-//      if(event.target.className === 'num-btn'){
-//         // for multidigit: check if the previous entry was a number
-//         if(typeof lineUp[lineUp.length -1] === 'number'){
-//             let previousNumber = lineUp[lineUp.length-1];
-//             let multiDigit = previousNumber.toString() + currentTarget;
-//             //replace the previewus number with the generated multidigit number
-//             lineUp.splice(-1, 1, Number(multiDigit)); 
-//         } else if(typeof currentNumber === 'string' && currentNumber.charAt(currentNumber.length-1) === '.'){
-//             let decimal = currentNumber + currentTarget;
-//             lineUp.splice(-1, 1, parseFloat(decimal))  
-//         } else {
-//             lineUp.push(Number(currentTarget));
-//         }
-//         //the current number will be stored in an object
-//         operationElements.number = lineUp[lineUp.length -1];
-//         display.innerText = operationElements.number;
-//         //what if a operator button is pressed
-//      } else if(event.target.className === 'operator') {
-//          if(result !== null && operationElements.number === null){
-//              lineUp.push(result);
-//          }
-         
-//          // Edge case: user is pressing multiple operators in a row
-//          if(typeof lineUp[lineUp.length -1] !== 'number'){
-//              lineUp.splice(-1, 1, currentTarget)
-//          } else {
-//              lineUp.push(currentTarget);
-//          }
-//          operationElements.operator = currentTarget;
-
-//         //what number shuld be shown after the operator is pressed
-//          if(lineUp.length < 3){
-//             display.innerText = lineUp[0];
-//          } else {
-//             display.innerText = result;
-//          }
-//      } else if(event.target.className === 'negative'){
-//          if(operationElements.number < 0){
-//             operationElements.number = - operationElements.number
-//          } else {
-//             operationElements.number = operationElements.number * (-1);
-//          }
-//             lineUp.splice(-1, 1, operationElements.number);
-//             display.innerText = operationElements.number
-
-//      } else if(event.target.className === 'decimals'){
-//         operationElements.number = operationElements.number + '.'
-//         display.innerText = operationElements.number
-//      } else if(event.target.className === 'percent'){
-//         operationElements.number = operationElements.number/100;
-//         lineUp.splice(-1, 1, operationElements.number);
-//         display.innerText = operationElements.number;
-//      } else if(event.target.className === 'delete'){
-//          let adjNumber = currentNumber.toString();
-//          operationElements.number = Number(adjNumber.slice(0, -1));
-//          lineUp.splice(-1, 1, operationElements.number)
-//          display.innerText = operationElements.number;
-//      } else if(event.target.className === 'clear'){
-//          clear()
-//      } 
-
-//      if(currentTarget === '='){
-//         lineUp = [];
-//         operationElements = {operator: null, number: null};
-//      }
-
-//      if(lineUp.length === 3){
-//         operate(operationElements.operator, lineUp[0], lineUp[2]);
-//      } else if(lineUp.length > 3 && event.target.className === 'num-btn'){
-//         operate(operationElements.operator, result, lineUp[lineUp.length -1]);
-//      }
-//     });
-// })
