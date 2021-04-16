f0e317c2687059bfafa8ca8a6b9c5f967e9e4241
susi189 <ssteinhoff82@gmail.com>
2021-04-16 13:15:34 -0400

    Added a 9 digit limit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Create funtions for each operation
+// In this function I perform the calculations
 
 const operate = function(operator, num1, num2){ 
     const firstNum = parseFloat(num1);
@@ -17,11 +17,19 @@ const operate = function(operator, num1, num2){
     }
 }
 
+const setNumOfDigits = (displayedContent) => {
+    if(displayedContent.length > 9){
+        return displayedContent.slice(0, 9);
+    } else {
+        return displayedContent;
+    }
+}
+
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.keys');
 const display = document.querySelector('.display');
 
-
+//Improvement options for consider in the future: refactor the code to make the event listener more readable 
 
 keys.addEventListener('click', (event) => {
     if(event.target.matches('button')){
@@ -33,20 +41,43 @@ keys.addEventListener('click', (event) => {
         let firstValue = calculator.dataset.firstValue;
         let operator = calculator.dataset.operator;
         let secondValue = displayedContent;
-        console.log(previousKeyType, firstValue, operator, secondValue)
+        //If the key is a number
         if(!action){
-            //we have a number
-            if(!firstValue || previousKeyType === 'operator'){
+            if (displayedContent === '0' && !firstValue || previousKeyType === 'operator'){
                 display.innerText = keyContent;
             } else if(previousKeyType === 'calculate'){
                 display.innerText = keyContent;
                 calculator.dataset.firstValue = '';
                 calculator.dataset.operator = '';
+            } else if(displayedContent.length > 8){
+                display.innerText = displayedContent
             } else {
                 display.innerText = displayedContent + keyContent;
             }
              calculator.dataset.previousKeyType = 'number';
         } 
+        //Making numbers negative
+        if(action === 'negative'){ 
+            if(!displayedContent.includes('-') && displayedContent !== '0'){
+                display.innerText = '-' + displayedContent;
+            } else if(displayedContent.includes('-')) {
+                display.innerText = displayedContent.slice(1);
+            }
+            calculator.dataset.previousKeyType = 'negative';
+        }
+         if(action === 'decimal'){
+            if(displayedContent.length > 7){
+                display.innerText = displayedContent;
+            } else if(!displayedContent.includes('.')){
+                display.innerText = displayedContent + '.'
+            } 
+            if(previousKeyType === 'operator' || previousKeyType === 'equal'){
+                display.innerText = '0.'
+            }
+            calculator.dataset.previousKeyType = 'decimal';
+        }
+
+        //if the key is an operator for calculations
         if(action === 'add' || action === 'subtract' || action === 'divide' || action === 'multiply'){
             if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
                 const calculatedValue = operate(operator, firstValue, secondValue);
@@ -59,6 +90,7 @@ keys.addEventListener('click', (event) => {
             calculator.dataset.previousKeyType = 'operator';
             calculator.dataset.operator = action;
         }
+        //If the key is a equal sign
         if(action === 'calculate'){
             if(firstValue){
                 if(previousKeyType === 'calculate'){
@@ -70,45 +102,31 @@ keys.addEventListener('click', (event) => {
             calculator.dataset.modValue = secondValue;
             calculator.dataset.previousKeyType = 'calculate';
         }
+        //If the key is a AC sign
         if(action === 'clear'){
             display.innerText = '0';
-            calculator.dataset.firstValue = null;
-            calculator.dataset.modValue = null;
-            calculator.dataset.operator = null;
+            calculator.dataset.firstValue = '';
+            calculator.dataset.modValue = '';
+            calculator.dataset.operator = '';
             calculator.dataset.previousKeyType = 'clear';
         }
+        //If the key is a backspace sign
         if(action === 'delete'){
-            if(displayedContent !== '0'){
+            if(displayedContent.length > 1){
                 display.innerText = displayedContent.slice(0, -1)
+            } else {
+                display.innerText = '0'
             }
             calculator.dataset.previousKeyType = 'delete'
-        } 
-        if(action === 'decimal'){
-            if(!displayedContent.includes('.')){
-                display.innerText = displayedContent + '.'
-            } 
-            if(previousKeyType === 'operator' || previousKeyType === 'equal'){
-                display.innerText = '0.'
-            }
-            calculator.dataset.previousKeyType = 'decimal';
         }
+        // if the key is a % sign
         if(action === 'percent'){
             display.innerText = displayedContent/100;
             calculator.dataset.previousKeyType = 'percent';
         }
-        if(action === 'negative'){ 
-            if(!displayedContent.includes('-') && displayedContent !== '0'){
-                display.innerText = '-' + displayedContent;
-            } else if(displayedContent.includes('-')) {
-                display.innerText = displayedContent.slice(1);
-            }
-            calculator.dataset.previousKeyType = 'negative';
-        }
     }
-    
 });
 
 // Egde cases:
-// -> digit no longer than 9
 // -> is-depressed when user clicks the button
 
